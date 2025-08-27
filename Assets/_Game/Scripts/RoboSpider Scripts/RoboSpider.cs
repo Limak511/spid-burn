c49c7a344cd1c1bbe82818a5bfe339ab8972d0f9
Limak511 <kamilbacia96@gmail.com>
2025-08-27 16:52:18 +0200

    Add AI movement to RoboSpider.cs

--- Assets/_Game/Scripts/RoboSpider Scripts/RoboSpider.cs
+++ Assets/_Game/Scripts/RoboSpider Scripts/RoboSpider.cs
@@ -1,6 +1,41 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MovementAI))]
 public class RoboSpider : MonoBehaviour
 {
-    
+    [SerializeField] private float _patrolSpotVisionRange;
+    private MovementAI _movementAI;
+    private Rigidbody2D _rb2d;
+    private Vector2 _patrolSpot;
+    float _repathRate = .2f;
+
+
+
+    private void Awake()
+    {
+        _movementAI = GetComponent<MovementAI>();
+        _rb2d = GetComponent<Rigidbody2D>();
+        _patrolSpot = _movementAI.GetRandomSpotOnAStarGrid();
+    }
+
+    private void FixedUpdate()
+    {
+        _movementAI.Move(_rb2d, _patrolSpot, _repathRate);
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, _patrolSpot) < _patrolSpotVisionRange)
+        {
+            _patrolSpot = _movementAI.GetRandomSpotOnAStarGrid();
+        }
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, _patrolSpotVisionRange);
+    }
 }
