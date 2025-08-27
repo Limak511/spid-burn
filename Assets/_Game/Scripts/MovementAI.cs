ec9ac96ac2292eeed74eb1dc3471bc0cba83547d
Limak511 <kamilbacia96@gmail.com>
2025-08-28 01:01:43 +0200

    Update MovementAI.cs

--- Assets/_Game/Scripts/MovementAI.cs
+++ Assets/_Game/Scripts/MovementAI.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(Seeker))]
 public class MovementAI : MonoBehaviour
 {
+    //[Header("Local Avoidance")]
+    //[SerializeField] private float _maxSeeAhead = .5f;
+    //[SerializeField] private LayerMask _avoidLayer;
+    //[SerializeField] private float _avoidancePushAwayForce = 2f;
     [SerializeField] private MovementSO _movementData;
     [SerializeField] private float _nextWaypointDistance = .5f;
     private bool _reachedEndOfPath;
@@ -12,6 +16,7 @@ public class MovementAI : MonoBehaviour
     private Path path;
     private Seeker _seeker;
     private float _lastRepath = float.NegativeInfinity;
+    private Vector2 _direction;
 
     public void SetMovementData(MovementSO movementData)
     {
@@ -51,6 +56,12 @@ public class MovementAI : MonoBehaviour
 
 
 
+    /// <summary>
+    /// Move GameObject to target position. USE ONLY IN FIXED UPDATE
+    /// </summary>
+    /// <param name="rb">Rigidbody2D of GameObject we want to move</param>
+    /// <param name="targetPosition">Positon where we want to move GameObject</param>
+    /// <param name="repathRate">Path's update rate</param>
     public void Move(Rigidbody2D rb, Vector2 targetPosition, float repathRate = .4f)
     {
         if (Time.time > _lastRepath + repathRate && _seeker.IsDone())
@@ -86,8 +97,21 @@ public class MovementAI : MonoBehaviour
             }
         }
 
-        Vector2 direction = ((Vector2)path.vectorPath[_currentWaypoint] - (Vector2)transform.position).normalized;
-        _movementData.Move(rb, direction);
+        _direction = ((Vector2)path.vectorPath[_currentWaypoint] - (Vector2)transform.position).normalized;
+
+        // Local avoidance
+        //var hits = Physics2D.RaycastAll(transform.position, _direction, _maxSeeAhead, _avoidLayer);
+        //foreach (var hit in hits)
+        //{
+        //    if (hit.transform == transform)
+        //    {
+        //        continue;
+        //    }
+
+        //    _direction = (_direction - (Vector2)hit.transform.position).normalized * _avoidancePushAwayForce;
+        //}
+
+        _movementData.Move(rb, _direction);
     }
 
 
@@ -105,5 +129,17 @@ public class MovementAI : MonoBehaviour
         });
         var randomNode = gridNodes[Random.Range(0, gridNodes.Count)];
         return (Vector3)randomNode.position;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            // Local avoidance - see ahead vector
+            //Gizmos.color = Color.yellow;
+            //Gizmos.DrawRay(transform.position, _direction * _maxSeeAhead);
+        }
     }
 }
