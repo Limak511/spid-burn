6ed8e0f3c717336f9771b3c7e0790eda35b37274
Limak511 <kamilbacia96@gmail.com>
2025-08-31 00:42:08 +0200

    Add PlayerSpiderAttacher.cs

---
+++ Assets/_Game/Scripts/Player Scripts/PlayerSpiderAttacher.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerSpiderAttacher : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _attachedSpiders;
+    [SerializeField] private IntEvent _onSpiderAttached;
+    private int _currentSpiderCount = 0;
+
+    [SerializeField] private bool _canAttachSpidersDebug = true;
+
+    private void Awake()
+    {
+        foreach (var spider in _attachedSpiders)
+        {
+            spider.SetActive(false);
+        }
+    }
+
+    public void AttachSpider()
+    {
+        // When all spiders attached
+        if (!CanAttachSpider())
+        {
+            // Spread cocoon on player
+            Debug.Log("Spread cocoon");
+            return;
+        }
+
+        _attachedSpiders[_currentSpiderCount].SetActive(true);
+        _currentSpiderCount++;
+
+        // Update UI
+        _onSpiderAttached.Raise(_currentSpiderCount);
+    }
+
+    public bool CanAttachSpider()
+    {
+        return _currentSpiderCount < _attachedSpiders.Length && _canAttachSpidersDebug;
+    }
+}
